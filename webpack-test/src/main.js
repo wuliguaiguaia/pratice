1b28241fd4a57c84f90e01b959078d3db54c1b33
zhouyuanli <zhouyuanli@zuoyebang.com>
2021-01-20 13:15:05 +0800

    feat: border-image 测试

--- webpack-test/src/main.js
+++ webpack-test/src/main.js
@@ -1,2 +1,13 @@
-const a = 1;
+// import _ from 'lodash';
+import a from './print.js';
+
+document.body.innerHTML = a.a1;
+
 console.log(a);
+if (module.hot) {
+  module.hot.accept('./print.js', function (data) {
+    console.log('Accepting the updated printMe module!');
+    console.log(data);
+    console.log(a);
+  })
+}
