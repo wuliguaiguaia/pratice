c84953aea707f4645ac5cab685a56af53c0984d2
zhouyuanli <zhouyuanli@zuoyebang.com>
2021-08-04 18:08:43 +0800

    test

--- react-test/hooks/useEffect/main.js
+++ react-test/hooks/useEffect/main.js
@@ -1,37 +1,64 @@
+// effect（副作用）：指那些没有发生在数据向视图转换过程中的逻辑，如 ajax 请求、访问原生dom 元素、本地持久化缓存、绑定/解绑事件、添加订阅、设置定时器、记录日志等。
+// 副作用操作可以分两类：需要清除的和不需要清除的。
+// 原先在函数组件内（这里指在 React 渲染阶段）改变 dom 、发送 ajax 请求以及执行其他包含副作用的操作都是不被允许的，因为这可能会产生莫名其妙的 bug 并破坏 UI 的一致性
+// useEffect 就是一个 Effect Hook，给函数组件增加了操作副作用的能力。
+// 它跟 class 组件中的 componentDidMount、componentDidUpdate 和 componentWillUnmount 具有相同的用途，只不过被合并成了一个 API
+// useEffect 接收一个函数，该函数会在组件渲染到屏幕之后才执行，该函数有要求：要么返回一个能清除副作用的函数，要么就不返回任何内容
+// 与 componentDidMount 或 componentDidUpdate 不同，使用 useEffect 调度的 effect 不会阻塞浏览器更新屏幕，这让你的应用看起来响应更快。
+// 大多数情况下，effect 不需要同步地执行。在个别情况下（例如测量布局），有单独的 useLayoutEffect Hook 供你使用，其 API 与 useEffect 相同。
+
+
 function Test() {
+  effectCursor = 0 /* note */
   const [number, setNumber] = React.useState(0)
+  const [name, setName] = React.useState('p')
   const onClick = _ => setNumber(number + 1)
-  useEffect(() => {
-    console.log(22);
+  const onClickName = _ => setName(name + 'k')
+  useEffect((val) => {
+    console.log(val);
   }, [number])
+  useEffect((val) => {
+    console.log(val, '===');
+    
+      // useEffect 如果返回一个函数的话，该函数会在组件卸载和更新时调用
+      // useEffect 在执行副作用函数之前，会先调用上一次返回的函数
+      // 如果要清除副作用，要么返回一个清除副作用的函数
+  })
   return <div>
     {number}
     <br />
     <button onClick={onClick}>number</button>
+    <br />
+    {name}
+    <br />
+    <button onClick={onClickName}>name</button>
   </div>
 }
 
-let state
-function useEffect(cb, arr) {
-  if (!state) {
-    state = arr
-    cb()
+let alldeps = []
+let effectCursor = 0;
+function useEffect(cb, depArray) {
+  if (!depArray) { // 1、空，每次都执行
+    cb();
+    alldeps[effectCursor] = depArray
+    effectCursor++
     return
   }
 
-  let hasChange = state ? state.length ? arr.some((item, i) => item !== state[i]) : false : true;
-  if (hasChange) {
-    cb()
-    render()
+  let deps = alldeps[effectCursor]
+  let hasChangeDeps = deps
+    ? depArray.some((item, i) => item != deps[i]) // 2、空数组每次都相等，只执行一次
+    : true // 3、第一次执行
+  if (hasChangeDeps) {
+    cb(depArray)
+    alldeps[effectCursor] = depArray
   }
+  effectCursor++
 }
 
 
-function render() {
-  ReactDOM.render(
-    <Test>das</Test>,
-    document.querySelector('#root')
-  )
-}
+ReactDOM.render(
+  <Test>das</Test>,
+  document.querySelector('#root')
+)
 
-render()
--- react-test/hooks/useState/main.js
+++ react-test/hooks/useState/main.js
@@ -1,3 +1,18 @@
+/* 
+  React 假设当你多次调用 useState 的时候，你能保证每次渲染时它们的调用顺序是不变的。
+  通过在函数组件里调用它来给组件添加一些内部 state，React会 在重复渲染时保留这个 state
+  useState 唯一的参数就是初始 state
+  useState 会返回一个数组：一个 state，一个更新 state 的函数
+
+  类似 class 组件的 this.setState，但是它不会把新的 state 和旧的 state 进行合并，而是直接替换
+ */
+
+
+
+// 每次渲染都是独立的闭包
+// 每一次渲染都有它自己的 Props 和 State
+// 每一次渲染都有它自己的事件处理函数
+// 当点击更新状态的时候，函数组件都会重新被调用，那么每次渲染都是独立的，取到的值不会受后面操作的影响
 function Test() {
   let [number, setNumber] = useState(0)
   let [name, setName] = useState('q')
