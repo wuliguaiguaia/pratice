8182b43fb87b5709ee4c3349c10963d709a87d7f
zhouyuanli <zhouyuanli@zuoyebang.com>
2021-08-18 14:34:41 +0800

    optimize: 增加eslint校验

--- eslint-test/vue.config.js
+++ eslint-test/vue.config.js
@@ -1,3 +1,4 @@
+console.log(process.env.npm_package_name, '==');
 module.exports = {
   // lintOnSave: "default",
   // 设置为 true 或 'warning' 时，eslint- loader 会将 lint 错误输出为编译警告。默认情况下，警告仅仅会被输出到命令行，且不会使得编译失败。
--- test/index.js
+++ test/index.js
@@ -1,34 +1,12 @@
-const l = {
-  val: 1,
-  next: {
-    val: 3,
-    next: {
-      val: 3,
-      next: {
-        val: 3,
-        next: {
-          val: 9
-        }
-      }
-    }
-  }
-}
-function fn (phead) {
-  const p = { next: phead }
-  let p1 = p; let p2 = p.next
-  while (p2 && p2.next) {
-    if (p2.val === p2.next.val) {
-      while (p2.next && p2.next.val === p2.val) {
-        p2.next = p2.next.next
-      }
-      p1.next = p2.next
-      p2 = p2.next
-    } else {
-      p1 = p1.next
-      p2 = p2.next
-    }
-  }
-  return p.next
+function fn() {
+  throw new Object({
+    errCode: "1001",
+    message: "生成py文件失败",
+  })
 }
 
-console.log(JSON.stringify(fn(l)))
+try {
+  fn()
+} catch (err) {
+  console.log(err);
+}
