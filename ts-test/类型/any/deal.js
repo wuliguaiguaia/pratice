e4737198175d6fb1c5d78620255275416bfdea00
zhouyuanli <zhouyuanli@zuoyebang.com>
2021-11-17 23:56:15 +0800

    ts

---
+++ ts-test/类型/any/deal.js
@@ -0,0 +1,10 @@
+"use strict";
+/* eslint-disable */
+// noImplicitAny": true, // 不能写any
+var qaw = function (a, b) {
+    return a + b;
+};
+console.log(qaw('1', '2'));
+// 看报错的对象
+// 1、使用泛型
+// 2、使用断言
--- ts-test/类型/any/deal.ts
+++ ts-test/类型/any/deal.ts
@@ -1,10 +1,15 @@
-// noImplicitAny": true, // 不能写any
-const qa = (a, b) => {
-    console.log(2);
-};
+/* eslint-disable */
 
-qa(1, 2);
+// noImplicitAny": true, // 不能写any
+const qaw = (a: string, b: string):string[] => {
+    return [a + b, a + b + 'ew']
+}
+
+console.log(qaw('1', '2'));
+
+const aaaa:string[] = qaw.filter((item:string)=> item === '12ew')
+
 
 // 看报错的对象
-// 1、 使用泛型
+// 1、使用泛型
 // 2、使用断言
