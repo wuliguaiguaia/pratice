b2f800e60d31e3fc542de8c306c706c8150f4a76
zhouyuanli <zhouyuanli@zuoyebang.com>
2021-11-19 16:01:38 +0800

    test: 测试lint

--- react-blog/blog/pages/index.tsx
+++ react-blog/blog/pages/index.tsx
@@ -5,14 +5,15 @@ import { useState } from 'react'
 import Author from '../components/Author'
 import Footer from '../components/Footer'
 import { useRouter } from 'next/dist/client/router'
-import axios from 'axios'
-
-function Home(props) {
-  console.log(props);
-  
-  const [mylist, setMylist] = useState(
+import axios from 'axios'    
+// import dynamic from 'next/dynamic'
+// const Eeader = dynamic(import('../components/Header'))
+function Home(props) {    
+  console.log(props)                   
+      
+  const [mylist, setMylist] = useState (
     [
-      { title: '第一', context: 'desc', id: 1 },
+      { title : '第一', context: 'desc', id: 1 },
       { title: '第而', context: 'desc', id: 2 },
     ]
   )
@@ -24,34 +25,34 @@ function Home(props) {
     } )
   }
   return (
-  <>
-    <Head>
-      <title>Home</title> 
-    </Head>
-    <Header/>
-    <Row className="main" type="flex" justify="center">
-      <Col className="main-left" xs={24} sm={24} md={16} lg={18} xl={14}>
-        <List
-          header={<div>最新日志</div>}
-          dataSource={mylist}
-          itemLayout="vertical"
-          renderItem={item => (
-            <List.Item onClick={() => { routeChange(item.id)}}>
-              <div className="list-title">{item.title}</div>
-              <div className="list-context">{item.context}</div>
-              <div className="list-keys">
-                <span>2019-06-28</span>
-              </div>
-            </List.Item>
-          )}
-        />
-      </Col>
-      <Col className="main-right" xs={24} sm={24} md={7} lg={5} xl={5}>
-        <Author/>
-      </Col>
-    </Row>
-    <Footer/>
-  </>
+    <>
+      <Head>
+        <title>Home</title> 
+      </Head>
+      <Header/>
+      <Row className="main" justify="center">
+        <Col className="main-left" xs={24} sm={24} md={16} lg={18} xl={14}>
+          <List
+            header={<div>最新日志</div>}
+            dataSource={mylist}
+            itemLayout="vertical"
+            renderItem={item => (
+              <List.Item onClick={() => { routeChange(item.id)}}>
+                <div className="list-title">{item.title}</div>
+                <div className="list-context">{item.context}</div>
+                <div className="list-keys">
+                  <span>2019-06-28</span>
+                </div>
+              </List.Item>
+            )}
+          />
+        </Col>
+        <Col className="main-right" xs={24} sm={24} md={7} lg={5} xl={5}>
+          <Author/>
+        </Col>
+      </Row>
+      <Footer/>
+    </>
   )
 }
 
@@ -59,7 +60,7 @@ Home.getInitialProps = async () => {
   // const getData = await axios('https://www.easy-mock.com/mock/5cfcce489dc7c36bd6da2c99/xiaojiejie/getList')
   // const data = getData()
   // console.log(data);
-  return 'fsdfsdfsfsfsdfsdf'
+  return 'fsdfsdfsfsfsdfsdf'      
 }
 
 export default Home
