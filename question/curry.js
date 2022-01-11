db4057bf416c55d2cf5d4e11aad6839e59773cf7
zhouyuanli <zhouyuanli@zuoyebang.com>
2022-01-11 11:49:17 +0800

    test

---
+++ question/curry.js
@@ -0,0 +1,20 @@
+/*
+  function curry(???){
+      ???
+      return ???
+  }
+  var abc = function(a, b, c) {
+    return [a, b, c];
+  };
+
+  var curried = curry(abc);
+
+  curried(1)(2)(3);
+  // => [1, 2, 3]
+
+  curried(1, 2)(3);
+  // => [1, 2, 3]
+
+  curried(1, 2, 3);
+  // => [1, 2, 3]
+*/
